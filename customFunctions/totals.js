ca942eb264b00b6fc8687c365d0e131abd016e1d
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2020-08-03 22:35:51 -0400

    new navbar

--- customFunctions/totals.js
+++ customFunctions/totals.js
@@ -1,5 +1,23 @@
 const pool = require('../db');
 
+class UserShares {
+  constructor(symbol, userId) {
+    (this.symbol = symbol), (this.userId = userId), (this.totalShares = 0);
+  }
+
+  async calculatingTotalShares() {
+    this.totalShares = await pool.query(
+      'SELECT SUM (shares) AS total FROM portifolio WHERE userid=$1 AND symbol=$2',
+      [this.userId, this.symbol]
+    );
+    console.log(this.totalShares.rows);
+    return this;
+  }
+}
+
+const testUser = new UserShares('DIS', 2);
+testUser.calculatingTotalShares();
+
 function totalShares(userId, ticker) {
   return pool.query(
     'SELECT SUM (shares) AS total FROM portifolio WHERE userid=$1 AND symbol=$2',
