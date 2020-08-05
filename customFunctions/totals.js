3e8e08eab75387cc64ca7a83fc23c8d0d768d55b
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2020-08-04 20:47:44 -0400

    created average cost function

--- customFunctions/totals.js
+++ customFunctions/totals.js
@@ -15,14 +15,58 @@ class UserShares {
   }
 }
 
-const testUser = new UserShares('DIS', 2);
-testUser.calculatingTotalShares();
+// const testUser = new UserShares('DIS', 2);
+// testUser.calculatingTotalShares();
 
-function totalShares(userId, ticker) {
+function totalShares(userId, symbol) {
   return pool.query(
     'SELECT SUM (shares) AS total FROM portifolio WHERE userid=$1 AND symbol=$2',
-    [userId, ticker]
+    [userId, symbol]
   );
 }
+
+async function averageCost(userId, symbol) {
+  let gettingAvgPriceAndShares;
+  let subTotals = [];
+  let totalShares = 0;
+  let totalCost = 0;
+
+  //getting average price and number of shares for each buy transaction
+  try {
+    gettingAvgPriceAndShares = await pool.query(
+      "SELECT avg_price, shares FROM portifolio WHERE userid=$1 AND symbol=$2 AND type='buy'",
+      [userId, symbol]
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  //calculating total cost of each transaction and total number of shares
+  try {
+    for (let i = 0; i < gettingAvgPriceAndShares.rows.length; i++) {
+      subTotals[i] =
+        gettingAvgPriceAndShares.rows[i].avg_price *
+        gettingAvgPriceAndShares.rows[i].shares;
+      totalShares = totalShares + gettingAvgPriceAndShares.rows[i].shares;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  // adding the subtotals
+  try {
+    for (let i = 0; i < subTotals.length; i++) {
+      totalCost = totalCost + subTotals[i];
+    }
+    console.log(totalCost);
+  } catch (err) {
+    console.log(err);
+  }
+
+  let avg_price = totalCost / totalShares;
+  return avg_price;
+}
+
+averageCost(2, 'DIS');
 
 exports.totalShares = totalShares;
